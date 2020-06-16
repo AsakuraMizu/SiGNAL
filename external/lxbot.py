d363d7387b53107daacd28811f4af34220894d68
AsakuraMizu <0xWATERx0@gmail.com>
2020-06-16 17:32:12 +0800

    more plugins

---
+++ external/lxbot.py
@@ -0,0 +1,41 @@
+import asyncio
+
+from sibot import on_event, Event, scheduler, get_bot, MessageChain, At
+
+TEST_GROUP = 0
+REPORT_GROUP = 0
+AT_QQ = 0
+
+flag = 0
+
+
+@on_event('GroupMessage')
+async def _(ev: Event):
+    if ev['sender']['group']['id'] == TEST_GROUP:
+        if 'LxBot' in ev['message_chain'].extract_plain_text():
+            global flag
+            flag = 0
+
+
+@scheduler.scheduled_job('interval', minutes=1)
+async def _():
+    global flag
+    if flag:
+        if flag == 1:
+            await get_bot().send_group_message(target=REPORT_GROUP,
+                                               message_chain=MessageChain([At(AT_QQ), ' lxlx，软糖酱又不理我了']))
+            await get_bot().send_group_message(target=REPORT_GROUP, message_chain=MessageChain('/x stat'))
+            await asyncio.sleep(2)
+            await get_bot().send_group_message(target=REPORT_GROUP, message_chain=MessageChain('软——糖——酱——'))
+        else:
+            await get_bot().send_group_message(target=REPORT_GROUP,
+                                               message_chain=MessageChain(f'呜呜呜软糖酱已经{flag}分钟没理我了......'))
+    await get_bot().send_group_message(target=TEST_GROUP, message_chain=MessageChain('/x stat'))
+    flag += 1
+
+
+def init(config):
+    global TEST_GROUP, REPORT_GROUP, AT_QQ
+    TEST_GROUP = config['test_group']
+    REPORT_GROUP = config['report_group']
+    AT_QQ = config['at_qq']
---
+++ sibot/builtin/common/autoagree.py
@@ -0,0 +1,12 @@
+from sibot import on_event, get_bot, Event
+
+
+@on_event('NewFriendRequestEvent')
+async def _(ev: Event):
+    await get_bot().resp_new_friend(event_id=ev['event_id'], from_id=ev['from_id'], group_id=ev['group_id'], operate=0)
+
+
+@on_event('BotInvitedJoinGroupRequestEvent')
+async def _(ev: Event):
+    await get_bot().resp_bot_invited_join_group(event_id=ev['event_id'], from_id=ev['from_id'], group_id=ev['group_id'],
+                                                operate=0)
